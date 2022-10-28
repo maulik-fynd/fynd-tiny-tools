e0a67338a77637c0de386e2e674e67dfcb9ae422
maulikpipaliya <maulikpipaliya@gofynd.com>
2022-10-28 10:56:22 +0530

    Added confirmation

--- fynd-git-commit-script/git-commit.sh
+++ fynd-git-commit-script/git-commit.sh
@@ -68,31 +68,43 @@ commitMessage="ID: $jiraId; HOURS: $hours; DONE: $donePerc; $commitMessage"
 
 echo $commitMessage
 
-while getopts acpe flag; do
-  case "${flag}" in
-  a) addall='true' ;;
-  c) docommit='true' ;;
-  e) emptycommit='true' ;;
-  p) dopush='true' ;;
+while true; do
+  read -p "Continue for next operations? (yn) " yn
+  case $yn in
+  [Yy]*)
+
+    while getopts acpe flag; do
+      case "${flag}" in
+      a) addall='true' ;;
+      c) docommit='true' ;;
+      e) emptycommit='true' ;;
+      p) dopush='true' ;;
+      esac
+    done
+
+    if [ "$addall" = "true" ]; then
+      echo "executing > git add ."
+      git add .
+    fi
+
+    if [ "$docommit" = "true" ]; then
+      echo "executing > git commit -m \"$commitMessage\""
+      git commit -m "$commitMessage"
+    fi
+
+    if [ "$emptycommit" = "true" ]; then
+      echo "executing > git commit --allow-empty -m \"$commitMessage\""
+      git commit --allow-empty -m "$commitMessage"
+    fi
+
+    if [ "$dopush" = "true" ]; then
+      echo "executing > git push"
+      git push
+    fi
+
+    break
+    ;;
+  [Nn]*) break ;;
+  *) echo "Please answer yes or no." ;;
   esac
 done
-
-if [ "$addall" = "true" ]; then
-  echo "executing > git add ."
-  git add .
-fi
-
-if [ "$docommit" = "true" ]; then
-  echo "executing > git commit -m \"$commitMessage\""
-  git commit -m "$commitMessage"
-fi
-
-if [ "$emptycommit" = "true" ]; then
-  echo "executing > git commit --allow-empty -m \"$commitMessage\""
-  git commit --allow-empty -m "$commitMessage"
-fi
-
-if [ "$dopush" = "true" ]; then
-  echo "executing > git push"
-  git push
-fi
